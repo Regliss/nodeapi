ee5656a47a09aefd592d041242d0b6fc17a2d280
darkregliss <pomart.thomas1@gmail.com>
2021-03-02 09:34:22 +0100

    alls

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,35 +1,40 @@
 const Product = require('../models/product.model');
 
 exports.create = (req, res) => {
-	const product = new Product({
-		price: req.body.price,
-		title: req.body.title,
-		description: req.body.description,
-		image: req.body.image
-	});
+  const product = new Product({
+    title: req.body.title,
+    price: req.body.price,
+    description: req.body.description,
+    image: req.body.image,
+  });
 
-	product.save()
-	.then((data) => {
-		res.send({
-			product:data,
-			created: true
-		})
-	})
-	.catch((err) => {
-		console.log(error);
-		res.status(500).send({
-			error: 500,
-			message: err.message || "some error occured"
-		})
-	})
-}
-
+  product
+    .save()
+    .then((data) => {
+      res.send({
+        product: data,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        error: 500,
+        message: err.message || 'some error occured while creating user',
+      });
+    });
+};
 
 exports.getProduct = (req, res) => {
-	Product.findById(req.params.id)
-	.then((data) => res.send(data))
-	.catch((err) =>	res.send(err));
-}
+  Product.findById(req.params.id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Product with id ${req.params.id} not found`,
+        });
+      }
+      res.send(data);
+    })
+    .catch((err) => res.send(err));
+};
 
 exports.getProducts = (req, res) => {
   Product.find()
--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const product = require('../controllers/products.controller');
 
-router.post('/products', product.create);
+router.post('/product', product.create);
 router.get('/products/', product.getProducts);
 router.get('/products/:id', product.getProduct);
 
